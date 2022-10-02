c7e9ae1021824b87704af3e169481bfe6fa86c6e
amitrappel <33310717+amitrappel@users.noreply.github.com>
2022-10-02 12:44:25 +0300

    Update two_boys.py

--- two_boys.py
+++ two_boys.py
@@ -7,17 +7,17 @@ days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Satur
 combos = list(product(genders, days))
 
 N = 10000
-conditional_history = [tup for tup in history if ('m', 'Tuesday') in tup]
+history = [((choice(genders), choice(days)), (choice(genders), choice(days))) for _ in range(N)]
 
 def is_two_boys(tup):
     return tup[0][0] == 'm' and tup[1][0] == 'm'
-    
+
 conditional_ps = []
 
 for n in range(100, N, 10):
-    conditional_history = [tup for tup in history if ('m', 'Tuesday') in tup]
+    conditional_history = [tup for tup in history[:n] if ('m', 'Tuesday') in tup]
     conditional_ps.append(sum([is_two_boys(tup) for tup in conditional_history]) / 
         len(conditional_history))
-        
+
 probs = pd.Series(conditional_ps, name='Conditional');
 probs.plot(xlabel='$N$ (Number of families tested)', ylabel='Probability of 2 boys');
