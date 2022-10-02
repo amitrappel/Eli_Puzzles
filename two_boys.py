a0d07e0146d7c5226c4a9686fb47402b7d73ce49
amitrappel <33310717+amitrappel@users.noreply.github.com>
2022-10-02 13:56:53 +0300

    Update two_boys.py

--- two_boys.py
+++ two_boys.py
@@ -1,25 +1,28 @@
-# from itertools import product
-# from random import choice
-# import pandas as pd
+from itertools import product
+from random import choice
+import pandas as pd
+import matplotlib.pyplot as plt
 
-# genders = ['m', 'f']
-# days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
-# combos = list(product(genders, days))
+genders = ['m', 'f']
+days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
+combos = list(product(genders, days))
 
-# N = 10000
-# history = [((choice(genders), choice(days)), (choice(genders), choice(days))) for _ in range(N)]
+N = 10000
+history = [((choice(genders), choice(days)), (choice(genders), choice(days))) for _ in range(N)]
 
-# def is_two_boys(tup):
-#     return tup[0][0] == 'm' and tup[1][0] == 'm'
+def is_two_boys(tup):
+    return tup[0][0] == 'm' and tup[1][0] == 'm'
 
-# conditional_ps = []
+conditional_ps = []
 
-# for n in range(100, N, 10):
-#     conditional_history = [tup for tup in history[:n] if ('m', 'Tuesday') in tup]
-#     conditional_ps.append(sum([is_two_boys(tup) for tup in conditional_history]) / 
-#         len(conditional_history))
+for n in range(100, N, 10):
+    conditional_history = [tup for tup in history[:n] if ('m', 'Tuesday') in tup]
+    conditional_ps.append(sum([is_two_boys(tup) for tup in conditional_history]) /
+        len(conditional_history))
 
-# probs = pd.Series(conditional_ps, name='Conditional');
-# probs.plot(xlabel='$N$ (Number of families tested)', ylabel='Probability of 2 boys');
+probs = pd.Series(conditional_ps, name='Conditional');
+fig, ax = plt.subplots(1, 1)
+probs.plot(xlabel='$N$ (Number of families tested)', ylabel='Probability of 2 boys', ax=ax);
+plt.show()
 
 print('aaa')
